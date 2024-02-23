c07bc965c0503d4db740ffefa14bfbc9af2bbc4c
ezequielleonzybert <ezequielleonzybert@gmail.com>
2024-02-23 02:01:36 -0300

    issue with platforms sync

--- game.js
+++ game.js
@@ -1,13 +1,25 @@
 let player
-let platform
+let platform = []
 
 class Game {
     setup() {
         player = new Player(canvas.width / 2, canvas.height / 2)
-        platform = new Platform(0, 0)
+        while (platform.length < 10)
+            platform.push(new Platform())
     }
     update(time) {
-        platform.set_position(time, 0)
+        for (let i = 0; i < platform.length; i++) {
+            const platform_width = platform[i].width
+            const origin_x = canvas.width + platform_width
+            const displace = -((platform[i].speed * time) % origin_x) + origin_x - platform_width / 2
+
+            platform[i].set_position(displace, platform[i].y)
+
+            const right_border = platform[i].x + platform_width
+            if (right_border < 0) {
+                platform[i].setup()
+            }
+        }
         player.update(time)
     }
 }
--- platform.js
+++ platform.js
@@ -1,19 +1,26 @@
 class Platform extends GameObject {
-    constructor(x, y) {
-        const shape = rectangle(0, 0, 200, 50)
+    constructor() {
+        const shape = rectangle(0, 0, canvas.width / 8, 30)
+        const y = Math.random() * 250
         super(
-            x,
+            0,
             y,
             shape,
             earcut(shape),
-            matrix.translation(x, y)
+            matrix.translation(0, y)
         )
+        this.width = canvas.width / 8
+        this.speed = Math.random() * 2 + 1
         game_object.push(this)
     }
-    update(time) {
-
+    setup() {
+        this.y = Math.random() * 250
+        this.speed = Math.random() * 2 + 1
     }
     set_position(x, y) {
-        super.transform = matrix.translation(x, y);
+        this.x = x
+        this.y = y
+        this.transform = matrix.translation(x, y);
+        // this.transofrm = matrix.multiply(translation, this.transform)
     }
 }
